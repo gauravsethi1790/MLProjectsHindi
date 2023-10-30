ea71c69668a19fab194d6c9231216f960a83ee57
Gaurav Sethi <gauravsethi1790@gmail.com>
2023-10-31 00:50:58 +0530

    model trainer

--- src/mlproject/components/data_ingestion.py
+++ src/mlproject/components/data_ingestion.py
@@ -25,7 +25,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         try:
             ##Reading code
-            df=read_sql_data()
+            df=pd.read_csv(os.path.join('notebook/data','raw.csv'))
             logging.info("Reading from mysql Database")
             
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
